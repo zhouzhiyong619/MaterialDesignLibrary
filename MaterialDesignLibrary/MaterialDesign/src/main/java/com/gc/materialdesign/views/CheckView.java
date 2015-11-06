9b41de16f9e77f3684d7cc1c1ec636f7b87357e2
zhouzhiyong <zhouzhiyong619@gmail.com>
2015-11-06 23:44:08 +0800

    change:修复锯齿问题

--- MaterialDesignLibrary/MaterialDesign/src/main/java/com/gc/materialdesign/views/CheckView.java
+++ MaterialDesignLibrary/MaterialDesign/src/main/java/com/gc/materialdesign/views/CheckView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
@@ -14,7 +15,7 @@ import com.gc.materialdesign.utils.Utils;
 /**
  * Created by zhouzhiyong on 15-11-6.
  */
-public class CheckView extends CheckBox{
+public class CheckView extends CustomView {
 
     final static String ANDROIDXML = "http://schemas.android.com/apk/res/android";
     private int srcId;
@@ -25,6 +26,7 @@ public class CheckView extends CheckBox{
     private Rect dstRect;
     private int widthOffest;
     private Paint srcPaint;
+    private int drawHeight;
 
     public CheckView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -36,15 +38,17 @@ public class CheckView extends CheckBox{
         setMinimumHeight(Utils.dpToPx(48, getResources()));
         setMinimumWidth(Utils.dpToPx(48, getResources()));
         srcId = attrs.getAttributeResourceValue(ANDROIDXML, "src", R.drawable.sprite_check);
-        Bitmap srcBitmap = BitmapFactory.decodeResource(getResources(), srcId);
-        mBitmap = Bitmap.createBitmap(15 * this.imgHeight, this.imgHeight, Bitmap.Config.ARGB_8888);
-        Rect localRect = new Rect(0, 0, this.mBitmap.getWidth(), this.mBitmap.getHeight());
-        srcPaint = new Paint();
-        srcPaint.setAntiAlias(true);
-        new Canvas(this.mBitmap).drawBitmap(srcBitmap, null, localRect, srcPaint);
+        mBitmap = BitmapFactory.decodeResource(getResources(), srcId);
+//        mBitmap = Bitmap.createBitmap(15 * this.imgHeight, this.imgHeight, Bitmap.Config.ARGB_8888);
+//        Rect localRect = new Rect(0, 0, this.mBitmap.getWidth(), this.mBitmap.getHeight());
+//        srcPaint = new Paint();
+//        srcPaint.setAntiAlias(true);
+//        new Canvas(this.mBitmap).drawBitmap(srcBitmap, null, localRect, srcPaint);
         srcRect = new Rect();
         dstRect = new Rect();
         widthOffest = 0;
+        setBackgroundColor(Color.TRANSPARENT);
+        drawHeight = Utils.dpToPx(20, getResources());
     }
 
     @Override
@@ -72,8 +76,10 @@ public class CheckView extends CheckBox{
     private void drawSrc(Canvas canvas) {
         canvas.save();
         srcRect = new Rect(imgHeight * widthOffest, 0, imgHeight * widthOffest + imgHeight, imgHeight);
-        dstRect = new Rect(getPaddingLeft(), getPaddingTop(), getWidth() - getPaddingRight(), getHeight() - getPaddingBottom());
+        //dstRect = new Rect(getPaddingLeft(), getPaddingTop(), getWidth() - getPaddingRight(), getHeight() - getPaddingBottom());
         //dstRect = new Rect(0, 0,getWidth(), getHeight());
+        dstRect = new Rect((getWidth() - drawHeight) / 2, (getHeight() - drawHeight) / 2,
+                (getWidth() + drawHeight) / 2, (getHeight() + drawHeight) / 2);
         canvas.drawBitmap(mBitmap, srcRect, dstRect, srcPaint);
         if (isChecked && widthOffest < 14) {
             widthOffest++;
